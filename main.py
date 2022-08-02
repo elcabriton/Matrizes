eff6f596235bb5e6532b6d17ca9195c515c41ae3
Heitor <mchheitor@gmail.com>
2022-08-01 23:10:59 -0300

    leitura fix

---
+++ main.py
@@ -0,0 +1,10 @@
+import multiprocessing
+import time 
+import pandas as pd
+
+#LER ARQUIVOS PRIMEIRO 
+with open("txt.txt","r") as arquivo:
+    matrizes=arquivo.read()
+    print (matrizes)#esta lendo o arquivo
+    
+
