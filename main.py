ba70f0eb227376dc5937bb174d9d278f049cdbc3
Heitor <mchheitor@gmail.com>
2022-08-07 20:28:12 -0300

    criação da função de ord por linha

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
 import multiprocessing
-import time 
+import time
 import pandas as pd
 
-#LER ARQUIVOS PRIMEIRO 
-with open("txt.txt","r") as arquivo:
-    matrizes=arquivo.read()
-    print (matrizes)#esta lendo o arquivo
-    
 
+# LER ARQUIVOS PRIMEIRO
+# • Matriz ordenada por linha
+# • Matriz ordenada por coluna
+# • Soma das linhas
+# • Soma das colunas
+# • Soma total da matriz
+# • Maiores de cada linha
+# • Maiores de cada coluna
+# • Menores de cada linha
+# • Menores de cada coluna
+
+
+
+
+def notacaocientifico_int(string_notacao):
+    string_formatada = string_notacao.replace('.', '').replace('e', '')
+    lista = string_formatada.split('+')
+    divisor = int(lista[0])
+    expoente = int(lista[1])
+    expoente = 10**expoente
+    resultado = expoente*divisor
+    return resultado
+
+
+def ord_linha(linhas):
+    lista_int = []
+
+    elementos = linhas.split("  ")
+
+    for elemento in elementos:
+        elemento_numerico = notacaocientifico_int(elemento)
+        lista_int.append(elemento_numerico)
+    linhas_ordenadas = sorted(lista_int)
+    return linhas_ordenadas
+
+
+arquivo = open('txt.txt', 'r')
+linhas = arquivo.readlines()
+# print(linhas)
+linhas_filtradas = []
+
+for x in linhas:
+
+    linhas_filtradas.append(x.strip().replace('\n', ''))
+arquivo_save = open('ordenado.txt', 'w')
+
+for linha in linhas_filtradas:
+    save = ord_linha(linha)
+    for x in save:
+        arquivo_save.write(str(x)+'  ')
+
+    arquivo_save.write('\n')
